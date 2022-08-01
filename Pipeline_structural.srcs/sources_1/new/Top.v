94733cfb4c333a3a6f6921f202f657c433e28918
Palade Bianca <56502232+Bianca66@users.noreply.github.com>
2022-08-01 09:48:43 +0300

    Update Top.v

--- Pipeline_structural.srcs/sources_1/new/Top.v
+++ Pipeline_structural.srcs/sources_1/new/Top.v
@@ -13,13 +13,13 @@ module top #(// General Parameters
     // Instruction Register
     reg [15:0] instr_reg;
     // Read to Execute Register
-    reg [91:0] rd2ex_reg;
+    reg [90:0] rd2ex_reg;
     // Execute to Write Back Register
     reg [36:0] ex2wb_reg;  
     // Register Set
     reg [D_SIZE-1:0] R [7:0];    
     // Memory
-    reg [D_SIZE-1:0] mem [A_SIZE^2-1:0];
+    reg [D_SIZE-1:0] mem [10:0];
     
     // Iterator
     integer i;
@@ -28,18 +28,13 @@ module top #(// General Parameters
     reg [D_SIZE-1:0] op1_rd;
     reg [D_SIZE-1:0] op2_rd;
     reg [D_SIZE-1:0] data_in_mem;
-    
-    // Wires
-    // Fetch
-    wire       [15:0] instr_ir;
+   
+
     // Read
     wire              en_write_pc;
-    wire [A_SIZE-1:0] addr_op1_df;
-    wire [A_SIZE-1:0] addr_op2_df;
-    wire              rs_en_df;
+    wire              reg_re_en_rs;
     wire              alu_en_rd2ex;
-    wire              shift_en_rd2ex;
-    wire              reg_we_rd2ex;
+    wire              reg_we_en_rd2ex;
     wire              mem_re_rd2ex;
     wire              mem_we_rd2ex;
     wire              loadc_en_rd2ex;
@@ -50,11 +45,14 @@ module top #(// General Parameters
     wire [D_SIZE-1:0] op2_rd2ex;
     wire        [2:0] dest_reg_rd2ex;
     wire        [6:0] alu_cmd_rd2ex;
+    wire        [2:0] addr_op2_rd2ex;
+    wire        [2:0] addr_op1_rd2ex;
+    
     // Execute
-    wire              jmp_sel_jc;
-    wire              jmpr_sel_jc;
-    wire [A_SIZE-1:0] jmp_jc;
-    wire [A_SIZE-1:0] jmp_offset_jc;
+    wire              jmp_sel_f;
+    wire              jmpr_sel_f;
+    wire [A_SIZE-1:0] jmp_f;
+    wire [A_SIZE-1:0] jmp_offset_f;
     wire              mem_re;
     wire              mem_we;
     wire [A_SIZE-1:0] addr_mem;
@@ -63,26 +61,19 @@ module top #(// General Parameters
     wire              load_en_ex2wb;
     wire [D_SIZE-1:0] result_ex2wb;
     wire        [2:0] dest_reg_ex2wb;
+    
+   
     // Write Back
-    wire [D_SIZE-1:0] result_df;
-    wire        [2:0] dest_df;
-    wire              reg_we_df;
-    // Data Forwarding
-    wire              clear_df;
-    wire              freeze_rd;
-    wire        [2:0] dest_rs;
     wire [D_SIZE-1:0] result_rs;
+    wire        [2:0] dest_rs;
     wire              reg_we_rs;
-    wire              reg_re_rs;
+        
+    wire        [2:0] addr_op2_rs;
     wire        [2:0] addr_op1_rs;
-    wire        [2:0] addr_op2_rs;  
-    // Jump Control
-    wire              clear_jc;
-    wire              jmp_sel_f;
-    wire              jmpr_sel_f;
-    wire [A_SIZE-1:0] jmp_f;
-    wire [A_SIZE-1:0] jmp_offset_f;
-         
+    
+    //Data Forwarding
+    wire              df1;
+    wire              df2;
     
     always @(posedge clk or negedge rst)
     begin
@@ -91,52 +82,85 @@ module top #(// General Parameters
             instr_reg <= 0;
             rd2ex_reg <= 0;
             ex2wb_reg <= 0;
-            for (i = 0; i <= 7; i = i + 1)
-            begin
-                R[i] <= 0;
-            end
         end
-        else if (clear_jc || clear_df)
+        else if(clear_rd)
         begin
-            instr_reg <= 0;
-            rd2ex_reg <= 0;
+           instr_reg <= {`NOP,    9'b000000000};
+           rd2ex_reg <= 0;
+           //ex2wb_reg <= 0;
+        end
+        else if(clear_df)
+        begin
+           instr_reg <= {`NOP,    9'b000000000};
+           rd2ex_reg <= 0;
+           ex2wb_reg <= 0;
         end
         else
         begin
-            instr_reg <= instr_ir;
-            rd2ex_reg <= {alu_en_rd2ex, shift_en_rd2ex, reg_we_rd2ex, mem_re_rd2ex, mem_we_rd2ex, loadc_en_rd2ex, jmp_sel_rd2ex, jmpr_sel_rd2ex, 
-                          jmp_offset_rd2ex, op1_rd2ex, op2_rd2ex, dest_reg_rd2ex, alu_cmd_rd2ex};
+            instr_reg <= instruction;
+            rd2ex_reg <= {alu_en_rd2ex, 
+                          reg_we_en_rd2ex, 
+                          mem_we_rd2ex, 
+                          mem_re_rd2ex, 
+                          loadc_en_rd2ex, 
+                          jmp_sel_rd2ex, 
+                          jmpr_sel_rd2ex, 
+                          jmp_offset_rd2ex, 
+                          op1_rd2ex, 
+                          op2_rd2ex, 
+                          alu_cmd_rd2ex,
+                          dest_reg_rd2ex 
+                          };
+                          
             ex2wb_reg <= {reg_we_ex2wb, load_en_ex2wb, result_ex2wb, dest_reg_ex2wb};
+        end
+    end
+    
+    always@(*)
+    begin
+        if(~rst)
+        begin
+            for (i = 0; i <= 7; i = i + 1)
+                begin
+                    R[i] <= 0;
+                end 
+            op1_rd <= 0;
+            op2_rd <= 0;
+        end
+        else 
+        begin
             if(reg_we_rs)
             begin
                 R[dest_rs] <= result_rs;
             end
-            else if(reg_re_rs)
+            if(reg_re_en_rs)
             begin
                 op1_rd <= R[addr_op1_rs];
                 op2_rd <= R[addr_op2_rs];
             end
-            else if(mem_re)
+            if(mem_re_rs)
                 data_in_mem <= mem[addr_mem];
-            else if(mem_we)
+            if(mem_we_rs)
                 mem[addr_mem] <= dout_mem;
         end
     end
     
-    fetch Fetch(// General
+    fetch  Fetch(// General
                .clk(clk),
                .rst(rst),
                // Inputs <- Program Memory
-               .instruction(instruction),
-               // Input <- Jump Control
-               .jmp_sel     (jmp_sel_jc),
-               .jmpr_sel    (jmpr_sel_jc),
-               .jmp         (jmp_jc),
-               .jmp_offset  (jmp_offset_jc),
+               //.instruction(instruction),
+               // Input <- EXECUTE
+               .jmp_sel     (jmp_sel_f),
+               .jmpr_sel    (jmpr_sel_f),
+               .jmp         (jmp_f),
+               .jmp_offset  (jmp_offset_f),
                // Input <- Read
                .en_write_pc (en_write_pc),
+               // Input <- Data Forwarding
+               .freeze      (clear_rd),
                // Output -> Instruction Register
-               .instr_ir    (instr_ir),
+               //.instr_ir    (instr_ir),
                // Output -> Program Memory
                .pc_out      (pc_out)
                );
@@ -149,21 +173,22 @@ module top #(// General Parameters
               // Input Reg Set
               .op1_rs      (op1_rd),
               .op2_rs      (op2_rd),
+              .reg_re_en   (reg_re_en_rs),
               // Input <- Data Forwarding + Jump Control
-              .clear       (clear_df || clear_jc),
-              // Input <- Data Forwarding
-              .freeze      (freeze_rd),
+              .clear       (clear_rd),
+              // Input <- DataForwarding
+              .df1         (df1),
+              .df2         (df2),
+              .result_ex   (result_ex2wb),
               // Output -> Fetch
               .en_write_pc (en_write_pc),
-              // Output -> Data Forwarding
-              .addr_op1    (addr_op1_df),
-              .addr_op2    (addr_op2_df),
-              .rs_en       (rs_en_df),
-              // Output -> Write Back
+              // Output -> Reg Set
+              .addr_op1    (addr_op1_rs),
+              .addr_op2    (addr_op2_rs),
+              // Output -> Read2EX
               //  - control signals
               .alu_en      (alu_en_rd2ex),
-              .shift_en    (shift_en_rd2ex),
-              .reg_we      (reg_we_rd2ex),
+              .reg_we_en   (reg_we_en_rd2ex),
               .mem_re      (mem_re_rd2ex),
               .mem_we      (mem_we_rd2ex),
               .loadc_en    (loadc_en_rd2ex),
@@ -184,37 +209,42 @@ module top #(// General Parameters
                     .rst          (rst),
                     // Input -> Rd2Ex
                     //     - control signals
-                    .alu_en_ex    (rd2ex_reg [91:91]),
-                    .shift_en_ex  (rd2ex_reg [90:90]),
-                    .reg_we_ex    (rd2ex_reg [89:89]),
-                    .mem_we_ex    (rd2ex_reg [88:88]),
-                    .mem_re_ex    (rd2ex_reg [87:87]),
-                    .loadc_en_ex  (rd2ex_reg [86:86]),
-                    .jmp_sel_ex   (rd2ex_reg [85:85]),
-                    .jmpr_sel_ex  (rd2ex_reg [84:84]),
+                    .alu_en_ex      (rd2ex_reg [90:90]),
+                    .reg_we_ex      (rd2ex_reg [89:89]),
+                    .mem_we_ex      (rd2ex_reg [88:88]),
+                    .mem_re_ex      (rd2ex_reg [87:87]),
+                    .loadc_en_ex    (rd2ex_reg [86:86]),
+                    .jmp_sel_ex     (rd2ex_reg [85:85]),
+                    .jmpr_sel_ex    (rd2ex_reg [84:84]),
                     //     - operands
-                    .jmp_offset_ex(rd2ex_reg [83:74]),
-                    .op1_ex       (rd2ex_reg [73:42]),
-                    .op2_ex       (rd2ex_reg [41:10]),
-                    .alu_cmd_ex   (rd2ex_reg [ 9:3]),
-                    .dest_reg_ex  (rd2ex_reg [ 2:0]),
-                    // Ouput -> Jump Control
-                    .jmp_sel      (jmp_sel_jc),
-                    .jmpr_sel     (jmpr_sel_jc),
-                    .jmp          (jmp_jc),
-                    .jmp_offset   (jmp_offset_jc),
+                    .jmp_offset_ex  (rd2ex_reg [83:74]),
+                    .op1_ex         (rd2ex_reg [73:42]),
+                    .op2_ex         (rd2ex_reg [41:10]),
+                    .alu_cmd_ex     (rd2ex_reg [ 9: 3]),
+                    .dest_reg_ex    (rd2ex_reg [ 2: 0]),
+                    
+                    // Input <- Data Forwarding
+                    //.clear(),
+                    // Ouput -> Fetch
+                    .jmp_sel        (jmp_sel_f),
+                    .jmpr_sel       (jmpr_sel_f),
+                    .jmp            (jmp_f),
+                    .jmp_offset     (jmp_offset_f),
                     // Output -> Memory
-                    .mem_re        (mem_re),
-                    .mem_we        (mem_we),
-                    .addr_mem      (addr_mem),
-                    .dout_mem      (dout_mem),
+                    .mem_re         (mem_re_rs),
+                    .mem_we         (mem_we_rs),
+                    .addr_mem       (addr_mem),
+                    .dout_mem       (dout_mem),
                     // Output -> Ex2wb Register
-                    .reg_we        (reg_we_ex2wb),
-                    .load_en       (load_en_ex2wb),
-                    .result        (result_ex2wb),
-                    .dest_reg      (dest_reg_ex2wb)
+                    .reg_we         (reg_we_ex2wb),
+                    .load_en        (load_en_ex2wb),
+                    .result         (result_ex2wb),
+                    .dest_reg       (dest_reg_ex2wb),
+                    .clear          (clear_rd)
                     );
-             
+    
+    //or(clear, clear_rd, clear_df);
+    
     write_back Write_Back (// General
                             .clk         (clk),
                             .rst         (rst),
@@ -225,68 +255,32 @@ module top #(// General Parameters
                             .dest_reg_wb (ex2wb_reg[ 2:0]),
                             // Input <- Memory 
                             .data_in     (data_in_mem),
-                            // Output -> Data Forwarding
-                            .result_rs   (result_df),
-                            .dest_rs     (dest_df),
-                            .reg_we_rs   (reg_we_df)
+                            // Output -> Register Set
+                            .result_rs   (result_rs),
+                            .dest_rs     (dest_rs),
+                            .reg_we_rs   (reg_we_rs)
                             );
-    
+                            
     data_forwarding Data_Forwarding (// General 
-                                     .clk         (clk),
-                                     .rst         (rst),    
-                                     // Input <- WRITE BACK
-                                     .reg_we      (reg_we_df),
-                                     .dest        (dest_df),
-                                     .result      (result_df),
-                                     // Input <- READ
-                                     .rs_en       (rs_en_df),
-                                     .addr_op1    (addr_op1_df),
-                                     .addr_op2    (addr_op2_df),
+                                     .clk           (clk),
+                                     .rst           (rst),  
+                                     // Input <- Read
+                                     .addr_op1_rd2ex(addr_op1_rs),
+                                     .addr_op2_rd2ex(addr_op2_rs),
+                                     .dest_rd2ex    (dest_reg_rd2ex),
+                                     .reg_re_en     (reg_re_en_rs),
+                                     // Input <- Execute
+                                     .dest_reg_wb   (dest_reg_ex2wb),
+                                     .reg_we_wb     (reg_we_rs),
+                                     .loadc_en      (load_en_ex2wb),
+                                     .mem_re_en     (mem_re_rs),
                                      // Output -> READ
-                                     .clear       (clear_df),
-                                     .freeze      (freeze_rd),
-                                     // Output -> Register Set
-                                     .dest_rs     (dest_rs),
-                                     .result_rs   (result_rs),
-                                     .reg_we_rs   (reg_we_rs),
-                                     .reg_re_rs   (reg_re_rs),
-                                     .addr_op1_rs (addr_op1_rs),
-                                     .addr_op2_rs (addr_op2_rs)    
-                             );
-                             
-    jump_control Jump_Control (// General
-                              .clk          (clk),
-                              .rst          (rst),
-                              // Input <- EXECUTE
-                              .jmp_sel      (jmp_sel_jc),
-                              .jmpr_sel     (jmpr_sel_jc),
-                              .jmp          (jmp_jc),
-                              .jmp_offset   (jmp_offset_jc),
-                              // Output -> READ, Instr Reg, Read2Ex 
-                              .clear_jc     (clear_jc),
-                              // Output -> FETCH
-                              .jmp_sel_f    (jmp_sel_f),
-                              .jmpr_sel_f   (jmpr_sel_f),
-                              .jmp_f        (jmp_f),
-                              .jmp_offset_f (jmp_offset_f)
-                              );
-                              
-       data_forwarding  Data_Forwarding (// General 
-                                         .clk(clk),
-                                         .rst(rst),
-                                         .addr_op1_rd(),
-                                         .addr_op2_rd(),
-                                         .op1_rd(), 
-                                         .op2_rd(),
-                                         .alu_en(),
-                                         .shift_en(),
-                                         .mem_re_en(),
-                                         .dest_ex(),
-                                         .result_ex(),
-                                         .clear(clear),
-                                         .freeze(freeze),
-                                         .op1_ex(),
-                                         .op2_ex()
-                         );
+                                     .clear         (clear_df),
+                                     .freeze        (freeze_df),
+                                     // Output -> Read
+                                     .df1           (df1),
+                                     .df2           (df2)
+                                     //.df_dest     (df_dest)
+                                     );
     
 endmodule
